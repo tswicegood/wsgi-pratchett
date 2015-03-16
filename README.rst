0c620911fa8b867eba1c27717eae65c5e187bc70
Travis Swicegood <development@domain51.com>
2015-03-16 16:33:02 -0700

    Copy in standard stuff needed for install

---
+++ README.rst
@@ -0,0 +1,16 @@
+============================
+GNU Terry Pratchett for WSGI
+============================
+
+Simple WSGI middleware for adding Terry Pratchett's header to your requests.
+
+
+-----
+Usage
+-----
+
+.. code-block: python
+    from pratchett import GNUTerryPratchett
+
+    # ... your code here, create your WSGI application
+    app = GNUTerryPratchett(your_app)
---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+if sys.argv[1] == 'develop':
+    from setuptools import setup
+else:
+    from distutils.core import setup
+
+setup(
+    name='wsgi-pratchett',
+    version='1.0.0',
+    author='Travis Swicegood',
+    author_email='development@domain51.com',
+    description='GNU Terry Pratchett for WSGI',
+    long_description=open("./README.rst").read(),
+    packages=['pratchett', ],
+    include_package_data=True,
+    zip_safe=False,
+)
