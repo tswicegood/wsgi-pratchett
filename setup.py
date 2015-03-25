5647862bbd846231be416d384cd32605148a7eaa
Travis Swicegood <development@domain51.com>
2015-03-25 14:59:08 -0700

    Switch to Python syntax for setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setup(
     packages=['pratchett', ],
     include_package_data=True,
     zip_safe=False,
-    entry_points="""
-    # -*- Entry points: -*-
-    [paste.filter_factory]
-    main = pratchett:make_filter
-    """,
+    entry_points={
+        "paste.filter_factory": [
+            "main = pratchett:make_filter",
+        ],
+    },
 )
